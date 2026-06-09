0946ef8537a70ad6585c21930f876eb99849aba2
Suraj <jagtapsuraj47@gmail.com>
2026-06-09 16:23:16 +0530

    Booking.java

--- src/main/java/com/main/entity/Booking.java
+++ src/main/java/com/main/entity/Booking.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -22,5 +25,13 @@ public class Booking {
 	private BookingStatus  bookingStatus;
 	
 	private List<String> seatNumber;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id",nullable = false)
+	private User users;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "show_id",nullable =false)
+	private Show show;
 	
 }
